8dcad135f2342d2d8ddba2fe2cb096d514845a7b
davidcoats <david@getsafetycone.com>
2023-02-10 17:52:12 -0800

    Interim changes.

--- source/R5T.X0003.X003/Code/Instances.cs
+++ source/R5T.X0003.X003/Code/Instances.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
 using R5T.T0037;
-using R5T.T0041;
 
 
 namespace R5T.X0003.X003
@@ -9,6 +8,6 @@ namespace R5T.X0003.X003
     public static class Instances
     {
         public static ICodeFileName CodeFileName { get; } = T0037.CodeFileName.Instance;
-        public static IPathOperator PathOperator { get; } = T0041.PathOperator.Instance;
+        public static F0002.IPathOperator PathOperator { get; } = F0002.PathOperator.Instance;
     }
 }
